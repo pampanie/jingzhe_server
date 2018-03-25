259ed3d05a829afdfe977665f5b075b74e674f82
pampa nie <nxg@artmusiclab.com>
2018-03-25 22:32:26 +0800

    start talk to jingzhe_client

--- jingzhe_server/src/ofApp.cpp
+++ jingzhe_server/src/ofApp.cpp
@@ -2,19 +2,78 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	
+	receiver.setup(PORT);
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	getAudioData();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
 }
+//--------------------------------------------------------------
+void ofApp::exit(){
+	
+//--------------------------------------------------------------
 
+}
+
+void ofApp::getAudioData(){
+	// check for waiting messages
+	while(receiver.hasWaitingMessages()){
+		// get the next message
+		ofxOscMessage m;
+		receiver.getNextMessage(m);
+		
+		// check for mouse moved message
+		if(m.getAddress() == RMS){
+			rms = m.getArgAsInt(0);
+		}
+		// check for mouse button message
+		else if(m.getAddress() == FFT){
+			// the single argument is a string
+			fftSize = m.getArgAsInt(0);
+			for (int i = 0; i < fftSize; i++) {
+				// 1st arg is fftsize
+				fft.push_back(m.getArgAsFloat(i+1));
+				cout << fft.at(i) << endl;
+			}
+	
+		}
+		else{
+			// unrecognized message: display on the bottom of the screen
+			string msg_string;
+			msg_string = m.getAddress();
+			msg_string += ": ";
+			for(int i = 0; i < m.getNumArgs(); i++){
+				// get the argument type
+				msg_string += m.getArgTypeName(i);
+				msg_string += ":";
+				// display the argument - make sure we get the right type
+				if(m.getArgType(i) == OFXOSC_TYPE_INT32){
+					msg_string += ofToString(m.getArgAsInt32(i));
+				}
+				else if(m.getArgType(i) == OFXOSC_TYPE_FLOAT){
+					msg_string += ofToString(m.getArgAsFloat(i));
+				}
+				else if(m.getArgType(i) == OFXOSC_TYPE_STRING){
+					msg_string += m.getArgAsString(i);
+				}
+				else{
+					msg_string += "unknown";
+				}
+			}
+			// ===========  need better handle error info =========  TODO
+			cout << msg_string << endl;
+		}
+		
+	}
+}
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
--- jingzhe_server/src/ofApp.h
+++ jingzhe_server/src/ofApp.h
@@ -1,24 +1,40 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOsc.h"
+
+// listen on port 12345
+#define PORT 12345
+
+// protocol for osc address
+#define FFT 	"/fft"
+#define RMS		"/rms"
+
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void getAudioData();
+	ofxOscReceiver 		receiver;
+	int					fftSize;
+	vector<float>		fft;
+	float				rms;
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
 };
